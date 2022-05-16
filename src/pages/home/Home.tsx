19af1a5ec23d7ee3d8eb98a4523061faa47b8da2
KaraboMaimane <maimane23@hotmail.com>
2022-05-16 22:32:07 +0200

    Basic layout of the product cards and adding local assets to the project

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -29,7 +29,7 @@ function Home() {
 			price: 150,
 			descripton:
 				'This is example text that will will serve as a placeholder for this product contained above. This is a great fragrance, spoil your man this fathers day now!',
-			imageUrl: './ship.png',
+			imageUrl: './cologne1.png',
 			category: 'fragrances',
 			categoryType: 'cologne',
 		},
@@ -41,7 +41,7 @@ function Home() {
 			price: 800,
 			descripton:
 				'This is the latest installment coming from South Africas very own Drip maker. This unisex sneaker will have heads turning as you rock them no doubt. Come on. You know you want to.',
-			imageUrl: './ship.png',
+			imageUrl: './shoe.png',
 			category: 'footwear',
 			categoryType: 'shoes',
 		},
@@ -53,7 +53,7 @@ function Home() {
 			price: 100,
 			descripton:
 				'This fragrance is oil based and will last for weeks on your clothing you spray once and you smell it forever. You want to get this for your partner trust me.',
-			imageUrl: './ship.png',
+			imageUrl: './cologne2.png',
 			category: 'perfumes',
 			categoryType: 'cologne',
 		},
@@ -65,23 +65,23 @@ function Home() {
 			price: 200,
 			descripton:
 				'This bucket hat is a must buy for anyone who loves i jean to jean. Add this to your arsenal and you wont regret it trust me.',
-			imageUrl: './ship.png',
+			imageUrl: './spoti.png',
 			category: 'clothing',
 			categoryType: 'hats',
 		},
 		{
-			manufacturer: 'Tshepo',
+			manufacturer: 'Tshepo The Jean Maker',
 			item: 'Jeans by Tshepo',
 			quantity: 0,
 			color: '',
 			price: 950,
 			descripton:
 				'These jeans were created by a local man in south africa who was aspiring to be like one of theses top designers',
-			imageUrl: './ship.png',
+			imageUrl: './jeans.png',
 			category: 'clothing',
 			categoryType: 'jeans',
 		},
-    {
+		{
 			manufacturer: 'Themba',
 			item: 'Stragen beanies',
 			quantity: 0,
@@ -89,7 +89,7 @@ function Home() {
 			price: 250,
 			descripton:
 				'These jeans were created by a local man in south africa who was aspiring to be like one of theses top designers',
-			imageUrl: './ship.png',
+			imageUrl: './beanie.png',
 			category: 'clothing',
 			categoryType: 'hats',
 		},
@@ -98,25 +98,32 @@ function Home() {
 	return (
 		<div className="w-full bg-gray-100">
 			<h1>Plug Majita Online Store</h1>
-			<p>The place where we connect gets with the latest fashion</p>
+			<p>The place where we plug gents with the latest fashion</p>
 			<section className="grid grid-cols-1 gap-6 w-8/12 md:grid-cols-3 md:w-10/12  lg:grid-cols-4 xl:grid-cols-6  sm:grid-cols-2 mx-auto">
 				{mockData.map((item, i) => (
-					<article key={i} className="h-56 flex flex-col py-4 px-4 bg-white  shadow-slate-400 shadow-sm  hover:shadow-lg cursor-pointer transition-shadow ease-in-out duration-300">
-						<figure className="h-20">
+					<article
+						key={i}
+						className="max-h-[20rem] flex flex-col py-4 px-4 rounded-md bg-white shadow-gray-600 drop-shadow-md  hover:shadow-lg cursor-pointer transition-shadow ease-in-out duration-300"
+					>
+						<figure className="h-[40%] relative">
 							<img
 								className="h-full mx-auto object-contain"
 								src={item.imageUrl}
 								alt={item.item}
 							/>
+
+              <p className='absolute bottom-0 right-0 bg-black px-2 text-white bg-[rgba(0,0,0,0.5)]'>R{item.price}</p>
 						</figure>
-						<div className="">
+						<div className="flex flex-col h-[60%] relative">
 							<div className="flex justify-between">
-								<p>{item.item}</p>
-								<p>{item.price}</p>
+								<p className="font-light text-lg text-left text-gray-800">{item.item}</p>
 							</div>
 							<div>
-								<p className='line-clamp-3'>{item.descripton}</p>
+								<p className="line-clamp-5 text-sm text-gray-600">{item.descripton}</p>
 							</div>
+              <div className='h-full flex'>
+              <a className='w-full self-end justify-self-start'>Checkout</a>
+              </div>
 						</div>
 					</article>
 				))}
